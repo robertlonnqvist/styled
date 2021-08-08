896e10bce0f71f612d75106e414150b5b739d995
Robert Lönnqvist <robert.lonnqvist@gmail.com>
2021-08-08 18:51:34 +0200

    Add more eslint plugins

--- src/components/About.tsx
+++ src/components/About.tsx
@@ -9,7 +9,7 @@ const About = (): JSX.Element => {
     >
       <p className="mb-3">
         <b>I love writing code</b> and being part of every aspect of the
-        software development process. I'm passionate about technologies and
+        software development process. I&#x27;m passionate about technologies and
         tools that make great products and increase developer productivity.
       </p>
       <p className="mb-3">
