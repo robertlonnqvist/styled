daf6672d590534dba088761f330719330e1def54
Robert Lönnqvist <robert.lonnqvist@gmail.com>
2021-09-11 17:49:23 +0200

    Fix default margin

--- src/components/Container.tsx
+++ src/components/Container.tsx
@@ -14,7 +14,7 @@ const Container = ({
   }, [title]);
 
   return (
-    <div className="container mx-auto max-w-4xl p-1 md:p-10">
+    <div className="container mx-auto max-w-4xl p-3 md:p-10">
       <Nav title={title} />
       <h1 className="text-2xl text-green-400 font-bold mb-3">{headline}</h1>
       {children}
